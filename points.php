7bb3a8f920104596ebf33fcaa86065b65527591c
Lawrence Norton <lnorton89@gmail.com>
2017-02-13 16:58:43 -0600

    Added Comments

--- points.php
+++ points.php
@@ -1,19 +1,22 @@
 <?php
+// Where we're pulling content from
 $url = 'http://www.nhra.com/standings/2017/nhra-mello-yello-drag-racing/top-fuel';
 
 $contents = file_get_contents($url); 
 
+// The container we'd like to pull
 $title = explode('<div class="tab-panels">',$contents); 
 $title = explode("</div>",$title[1]); 
 
+// Remove all tags but keep table structure
 $stripped_tags = strip_tags($title[0], '<div><table><td><tr><th>');
 
+// This will be removed when the drivers name is spelled right..
 $final = str_replace('Jimmy', 'Clay', $stripped_tags);
 
+// Where we'll be writing the data to
 $fp = fopen ("points.html", "w+"); 
 
 fwrite ($fp, "$final"); 
 fclose ($fp); 
-
-require_once("points.html");
 ?>
