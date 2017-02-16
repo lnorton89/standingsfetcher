f2a025479adf2373ff3fbd0e2cb71bc5fb264cfb
Lawrence Norton <lnorton89@gmail.com>
2017-02-15 18:50:52 -0600

    Removed Name Spell Check

--- points.php
+++ points.php
@@ -1,7 +1,6 @@
 <?php
 // Where we're pulling content from
 $url = 'http://www.nhra.com/standings/2017/nhra-mello-yello-drag-racing/top-fuel';
-
 $contents = file_get_contents($url); 
 
 // The container we'd like to pull
@@ -10,9 +9,6 @@ $title = explode("</div>",$title[1]);
 
 // Remove all tags but keep table structure
 $stripped_tags = strip_tags($title[0], '<div><table><td><tr><th>');
-
-// This will be removed when the drivers name is spelled right..
-$final = str_replace('Jimmy', 'Clay', $stripped_tags);
 
 // Where we'll be writing the data to
 $fp = fopen ("points.html", "w+"); 
